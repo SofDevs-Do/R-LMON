cddc0cc5a408226d2e3cb644b18133858c38642a
Sneha Rao GR <sneharaog@gmail.com>
2021-01-22 10:37:48 +0530

    Side-navigation menu front-end added.

--- web_frontend/static/js/rlmon_action.js
+++ web_frontend/static/js/rlmon_action.js
@@ -1,1 +1,9 @@
-// just a comment
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main-div").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main-div").style.marginLeft= "0";
+}
