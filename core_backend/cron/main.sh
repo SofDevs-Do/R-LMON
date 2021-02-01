40beb39b3d5818015ab5b6c9ccacc8d74382d678
Yashas ND <ndyashas@gmail.com>
2021-02-01 17:49:38 +0530

    Machine file now supports spaces between each of the fields.

--- core_backend/cron/main.sh
+++ core_backend/cron/main.sh
@@ -10,5 +10,13 @@ IFS=","
 grep -v '^#' < "${MACHINEFILE}" | \
     { while read REMOTE_MACHINE ROOM_ID RACK_ID MACHINE_LOCATION KVM_SWITCH KVM_NUMBER ALLOTED_TO COMMENTS
       do
-	  main "${REMOTE_MACHINE}" "${ROOM_ID}" "${RACK_ID}" "${MACHINE_LOCATION}" "${KVM_SWITCH}" "${KVM_NUMBER}" "${ALLOTED_TO}" "${COMMENTS}";
+	  main \
+	      "$(echo ${REMOTE_MACHINE} | xargs)" \
+	      "$(echo ${ROOM_ID} | xargs)" \
+	      "$(echo ${RACK_ID} | xargs)" \
+	      "$(echo ${MACHINE_LOCATION} | xargs)" \
+	      "$(echo ${KVM_SWITCH} | xargs)" \
+	      "$(echo ${KVM_NUMBER} | xargs)" \
+	      "$(echo ${ALLOTED_TO} | xargs)" \
+	      "$(echo ${COMMENTS} | xargs)";
       done; }
