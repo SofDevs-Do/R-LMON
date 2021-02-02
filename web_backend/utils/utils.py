324ad7c64717068a07d47e39dc33d9cf46d91305
Yashas ND <ndyashas@gmail.com>
2021-02-02 11:33:36 +0530

    End point for getting overview data added. Need to fix the db for this.

--- web_backend/utils/utils.py
+++ web_backend/utils/utils.py
@@ -1,5 +1,6 @@
 import os
 import errno
+from utils.rlmon_db import RLMON_DB
 
 class Util:
 
@@ -8,10 +9,40 @@ class Util:
     log_dir = os.path.join(rlmon_path, "core_backend", "_log")
 
     def __init__(self):
-
         # Check if the static data members are correct.
         if (not os.path.isdir(Util.rlmon_path)):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), Util.rlmon_path)
 
         if (not os.path.isdir(Util.log_dir)):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), Util.log_dir)
+
+        self.db = RLMON_DB(Util.rlmon_path, Util.log_dir)
+
+
+    def get_overview_data(self, start_date, end_date):
+        """
+        Returns the overview data.
+        {
+            'room_id':
+                 {
+                     'room_name': <string:name>,
+                     'rack_list':
+                         {
+                             'rack_id':
+                                   {
+                                       'rack_name': <string:name>,
+                                       'machines_list': ['machine_id_1', 'machine_id_2', ...]
+                                   },
+                              ...
+                         },
+                     ...
+                 },
+            ...
+        }
+        """
+        rooms_dict = self.db.get_rooms(start_date)
+
+        for room in rooms_dict:
+            rooms_dict[room]['rack_list'] = self.db.get_racks_in_room(start_date, room)
+
+        return rooms_dict
