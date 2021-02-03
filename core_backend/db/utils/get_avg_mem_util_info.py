01401a4cb73fe6783dae9f4530f87c44645095e5
Sneha Rao GR <sneharaog@gmail.com>
2021-02-03 15:37:55 +0530

    [BugFix] db: updated the avg_cpu_util and avg_mem_util to include date.

--- core_backend/db/utils/get_avg_mem_util_info.py
+++ core_backend/db/utils/get_avg_mem_util_info.py
@@ -3,9 +3,10 @@ import os
 AVG_MEM_UTIL_INFO_FILE_NAME = "avg-mem-usage.txt" # name of the filename in the log_path
 
 def get_avg_mem_util_info_dict(log_path, machine, avg_mem_util_info_key):
+    t_date = os.path.basename(log_path)
     avg_mem_util_info_file = os.path.join(log_path, machine, AVG_MEM_UTIL_INFO_FILE_NAME)
     with open(avg_mem_util_info_file, "r") as avg_mem_util_info_fp:
         avg_mem_util_info = float(avg_mem_util_info_fp.read().strip())
-    return {avg_mem_util_info_key : avg_mem_util_info}
+    return {avg_mem_util_info_key : {t_date : avg_mem_util_info}}
     
     
